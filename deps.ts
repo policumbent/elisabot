716727d01b50c70233e9b151f5fa3dcbd0c1b185
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2022-02-22 00:30:14 +0100

    fix: deno warning for desp versions

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * from "https://deno.land/x/grammy/mod.ts";
-export * from "https://deno.land/x/grammy_menu/mod.ts";
+export * from "https://deno.land/x/grammy@v1.7.0/mod.ts";
+export * from "https://deno.land/x/grammy_menu@v1.0.4/mod.ts";
 
 // load .env variables
-import "https://deno.land/x/dotenv/load.ts";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
